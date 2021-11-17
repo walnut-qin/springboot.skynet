0ae89b75bf116495dded38093e88e6ae934279ef
Pengzhan.Qin <935419854@qq.com>
2021-11-17 10:55:22 +0800

    first webApi

--- src/main/java/com/kaos/his/Controller/EmployeeController.java
+++ src/main/java/com/kaos/his/Controller/EmployeeController.java
@@ -8,18 +8,18 @@ import com.kaos.his.enums.util.GsonEnumTypeAdapter;
 import com.kaos.his.service.EmployeeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/testBoot")
-public class UserController {
+@RequestMapping("/webApi")
+public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @RequestMapping("getUser/{emplCode}")
-    public String GetUser(@PathVariable String emplCode) {
+    @RequestMapping("getEmployee")
+    public String GetEmployee(@RequestParam("emplCode") String emplCode) {
         var employee = this.employeeService.GetEmployeeByEmplCode(emplCode);
         Gson gson = new GsonBuilder().serializeNulls()
                 .registerTypeAdapter(SexEnum.class, new GsonEnumTypeAdapter<>(SexEnum.class))
