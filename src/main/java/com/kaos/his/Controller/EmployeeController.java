bf651f905f85907f8db98b3a36d75dbe35b9e078
Pengzhan.Qin <935419854@qq.com>
2021-11-17 15:56:28 +0800

    统一接口格式

--- src/main/java/com/kaos/his/Controller/EmployeeController.java
+++ src/main/java/com/kaos/his/Controller/EmployeeController.java
@@ -16,16 +16,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/webApi")
 public class EmployeeController {
+    /**
+     * Mapper映射
+     */
     @Autowired
     private EmployeeService employeeService;
 
+    /**
+     * 获取职工信息
+     * 
+     * @param emplCode 职工编码
+     * @return 职工信息json
+     */
     @RequestMapping(value = "getEmployee", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     public String GetEmployee(@RequestParam("emplCode") String emplCode) {
+        // 调取service获取职员实体
         var employee = this.employeeService.GetEmployeeByEmplCode(emplCode);
+
+        // 创建特定类型的gson对象
         Gson gson = new GsonBuilder().serializeNulls()
                 .registerTypeAdapter(SexEnum.class, new GsonEnumTypeAdapter<>(SexEnum.class))
                 .registerTypeAdapter(DeptOwnEnum.class, new GsonEnumTypeAdapter<>(DeptOwnEnum.class))
                 .setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+
         return gson.toJson(employee);
     }
 }
