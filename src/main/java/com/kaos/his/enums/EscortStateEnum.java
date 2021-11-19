5e606665a0a873a993ec44188a15744b495b37cb
Pengzhan.Qin <935419854@qq.com>
2021-11-19 10:31:21 +0800

    添加陪护人枚举

---
+++ src/main/java/com/kaos/his/enums/EscortStateEnum.java
@@ -0,0 +1,39 @@
+package com.kaos.his.enums;
+
+import com.kaos.his.enums.util.IEnum;
+
+public enum EscortStateEnum implements IEnum<EscortStateEnum> {
+    无核酸检测结果("0", "无核酸检测结果"), 等待院内核酸检测结果("1", "等待院内核酸检测结果"), 等待院外核酸检测结果审核("2", "等待院外核酸检测结果审核"), 生效中("3", "生效中"),
+    注销("4", "注销");
+
+    /**
+     * 数据库存值
+     */
+    private String value;
+
+    /**
+     * 描述存值
+     */
+    private String description;
+
+    /**
+     * 构造
+     * 
+     * @param index
+     * @param description
+     */
+    EscortStateEnum(String index, String description) {
+        this.value = index;
+        this.description = description;
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+}
