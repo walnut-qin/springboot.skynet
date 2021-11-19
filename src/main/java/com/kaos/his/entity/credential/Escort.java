658c5b6b9c960a8370257141aeba6bd91dcfccbe
Pengzhan.Qin <935419854@qq.com>
2021-11-19 10:59:45 +0800

    添加陪护实体

---
+++ src/main/java/com/kaos/his/entity/credential/Escort.java
@@ -0,0 +1,80 @@
+package com.kaos.his.entity.credential;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.kaos.his.entity.personnel.Patient;
+import com.kaos.his.enums.EscortActionEnum;
+import com.kaos.his.enums.EscortStateEnum;
+
+public class Escort {
+    /**
+     * 陪护证状态
+     */
+    public class EscortState {
+        /**
+         * 状态记录号
+         */
+        public Integer recNo;
+
+        /**
+         * 状态值
+         */
+        public EscortStateEnum state;
+
+        /**
+         * 状态记录生成时间
+         */
+        public Date operDate;
+    }
+
+    /**
+     * 陪护证行为
+     */
+    public class EscortAction {
+        /**
+         * 状态记录号
+         */
+        public Integer recNo;
+
+        /**
+         * 状态值
+         */
+        public EscortActionEnum action;
+
+        /**
+         * 状态记录生成时间
+         */
+        public Date operDate;
+    }
+
+    /**
+     * 陪护证编号
+     */
+    public String escortNo;
+
+    /**
+     * 关联的住院证
+     */
+    public HospitalizationCertificate hospitalizationCertificate;
+
+    /**
+     * 关联的患者
+     */
+    public Patient patient;
+
+    /**
+     * VIP标识
+     */
+    public boolean vip;
+
+    /**
+     * 状态清单
+     */
+    public List<EscortState> states;
+
+    /**
+     * 行为清单
+     */
+    public List<EscortAction> actions;
+}
