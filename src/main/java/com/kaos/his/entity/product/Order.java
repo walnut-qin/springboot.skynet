ffe2d43543ea9d120611428a0d294143f085ec66
Pengzhan.Qin <935419854@qq.com>
2021-11-23 17:32:47 +0800

    添加医嘱实体

---
+++ src/main/java/com/kaos/his/entity/product/Order.java
@@ -0,0 +1,30 @@
+package com.kaos.his.entity.product;
+
+import java.util.Date;
+
+public class Order {
+    /**
+     * 医嘱流水号
+     */
+    public String moOrder = null;
+
+    /**
+     * 开立医嘱时间
+     */
+    public Date moDate = null;
+
+    /**
+     * 索引号，门诊为卡号，住院为住院号
+     */
+    public String indexNo = null;
+
+    /**
+     * 项目编码
+     */
+    public String termId = null;
+
+    /**
+     * 项目名称
+     */
+    public String termName = null;
+}
