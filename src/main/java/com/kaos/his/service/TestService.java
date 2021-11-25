d85f82b4858a49984a0af65821c70bb3c3816abe
Pengzhan.Qin <935419854@qq.com>
2021-11-25 12:04:25 +0800

    完成查询接口

--- src/main/java/com/kaos/his/service/TestService.java
+++ src/main/java/com/kaos/his/service/TestService.java
@@ -1,10 +1,7 @@
 package com.kaos.his.service;
 
-import java.util.Date;
-
-import com.kaos.his.entity.credential.EscortCard;
-import com.kaos.his.enums.EscortStateEnum;
-import com.kaos.his.mapper.credential.EscortCardMapper;
+import com.kaos.his.entity.drug.DrugBaseInfo;
+import com.kaos.his.mapper.drug.DrugBaseInfoMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,25 +10,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class TestService {
     @Autowired
-    EscortCardMapper escortCardMapper;
+    DrugBaseInfoMapper drugBaseInfoMapper;
 
     @Transactional
-    public EscortCard Run(String param) {
-        var escort = this.escortCardMapper.QueryEscort(param);
-
-        // 取最后一个状态
-        var lstState = escort.states.get(escort.states.size() - 1);
-
-        // 定义一个新状态
-        var newState = new EscortCard.EscortState();
-        newState.escortNo = lstState.escortNo;
-        newState.recNo = lstState.recNo + 1;
-        newState.state = EscortStateEnum.注销;
-        newState.operDate = new Date();
-
-        // 插入状态
-        this.escortCardMapper.UpdateEscortState(newState);
-        escort.states.add(newState);
+    public DrugBaseInfo Run(String param) {
+        var escort = this.drugBaseInfoMapper.QueryDrugBaseInfo(param);
 
         return escort;
     }
