2393f3b99f11603f71f83c242dc9c008dbcaa3cb
Pengzhan.Qin <935419854@qq.com>
2021-12-18 14:43:53 +0800

    独立陪护证模块

--- src/main/java/com/kaos/his/entity/credential/EscortCard.java
+++ src/main/java/com/kaos/his/entity/credential/EscortCard.java
@@ -4,60 +4,8 @@ import java.util.Date;
 import java.util.List;
 
 import com.kaos.his.entity.personnel.Patient;
-import com.kaos.his.enums.EscortActionEnum;
-import com.kaos.his.enums.EscortStateEnum;
 
 public class EscortCard {
-    /**
-     * 陪护证状态
-     */
-    public static class EscortState {
-        /**
-         * 陪护证编号
-         */
-        public String escortNo = null;
-
-        /**
-         * 状态记录号
-         */
-        public Integer recNo = null;
-
-        /**
-         * 状态值
-         */
-        public EscortStateEnum state = null;
-
-        /**
-         * 状态记录生成时间
-         */
-        public Date operDate = null;
-    }
-
-    /**
-     * 陪护证行为
-     */
-    public static class EscortAction {
-        /**
-         * 陪护证编号
-         */
-        public String escortNo = null;
-
-        /**
-         * 状态记录号
-         */
-        public Integer recNo = null;
-
-        /**
-         * 状态值
-         */
-        public EscortActionEnum action = null;
-
-        /**
-         * 状态记录生成时间
-         */
-        public Date operDate = null;
-    }
-
     /**
      * 陪护证编号
      */
@@ -79,6 +27,16 @@ public class EscortCard {
     public String helperCardNo = null;
 
     /**
+     * 添加记录的时间戳
+     */
+    public Date operDate = null;
+
+    /**
+     * 备注
+     */
+    public String remark = null;
+
+    /**
      * 关联的住院证
      */
     public PreinCard preinCard;
@@ -91,10 +49,10 @@ public class EscortCard {
     /**
      * 状态清单
      */
-    public List<EscortState> states;
+    public List<EscortCardState> states;
 
     /**
      * 行为清单
      */
-    public List<EscortAction> actions;
+    public List<EscortCardAction> actions;
 }
--- src/main/java/com/kaos/his/mapper/credential/EscortCardMapper.java
+++ src/main/java/com/kaos/his/mapper/credential/EscortCardMapper.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EscortCardMapper {
     /**
-     * 获取陪护证
+     * 获取陪护证，主键查询
      * 
      * @param escortNo 陪护证编号
      * @return
@@ -17,62 +17,31 @@ public interface EscortCardMapper {
     EscortCard QueryEscort(String escortNo);
 
     /**
-     * 根据陪护人卡号查询关联的陪护证
+     * 查询陪护人关联的陪护证列表
      * 
-     * @param escortNo 陪护人卡号
-     * @return
+     * @param helperCardNo 陪护人卡号
+     * @return 陪护证列表
      */
     List<EscortCard> QueryHelperEscorts(String helperCardNo);
 
     /**
-     * 根据患者卡号查询关联的陪护证
+     * 查询患者住院证关联的陪护证列表
      * 
-     * @param cardNo
+     * @param patientCardNo 患者卡号
+     * @param happenNo      住院证编号
      * @return
      */
     List<EscortCard> QueryPatientEscorts(String patientCardNo, Integer happenNo);
 
     /**
-     * 指定陪护人和患者，查询历史陪护证
+     * 查询陪护历史
      * 
-     * @param patientCardNo
-     * @param happenNo
-     * @param helperCardNo
-     * @return
+     * @param patientCardNo 患者卡号
+     * @param happenNo      住院证编号
+     * @param helperCardNo  陪护人卡号
+     * @return 陪护证列表
      */
-    List<EscortCard> QueryHistoryEscorts(String patientCardNo, Integer happenNo, String helperCardNo);
-
-    /**
-     * 更新陪护证的状态
-     * 
-     * @param escortNo 陪护证编号
-     * @param newState 新状态
-     */
-    void InsertEscortState(EscortCard.EscortState newState);
-
-    /**
-     * 更新陪护证的状态
-     * 
-     * @param escortNo 陪护证编号
-     * @param newState 新状态
-     */
-    void InsertEscortStates(List<EscortCard.EscortState> newStates);
-
-    /**
-     * 更新陪护证的状态
-     * 
-     * @param escortNo 陪护证编号
-     * @param newState 新状态
-     */
-    void InsertEscortAction(EscortCard.EscortAction newAction);
-
-    /**
-     * 更新陪护证的状态
-     * 
-     * @param escortNo 陪护证编号
-     * @param newState 新状态
-     */
-    void InsertEscortActions(List<EscortCard.EscortAction> newActions);
+    List<EscortCard> QueryEscortHistorys(String patientCardNo, Integer happenNo, String helperCardNo);
 
     /**
      * 添加一个新的陪护证
@@ -80,11 +49,4 @@ public interface EscortCardMapper {
      * @param escortCard
      */
     void InsertEscort(EscortCard escortCard);
-
-    /**
-     * 生成一个新的陪护证号
-     * 
-     * @return
-     */
-    String GenerateNewEscortNo();
 }
