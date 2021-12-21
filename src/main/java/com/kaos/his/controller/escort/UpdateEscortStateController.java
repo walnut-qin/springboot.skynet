e1d1d2e9161726d4648edfaefa6ed55bea81134a
Pengzhan.Qin <935419854@qq.com>
2021-12-21 15:38:58 +0800

    优化代码写法，添加自动更新状态log

--- src/main/java/com/kaos/his/controller/escort/UpdateEscortStateController.java
+++ src/main/java/com/kaos/his/controller/escort/UpdateEscortStateController.java
@@ -2,6 +2,7 @@ package com.kaos.his.controller.escort;
 
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.kaos.his.entity.credential.EscortCard;
@@ -26,9 +27,9 @@ public class UpdateEscortStateController {
     EscortService escortService;
 
     /**
-     * 锁资源
+     * 陪护证操作锁
      */
-    private List<Object> locks = new ArrayList<>() {
+    private List<Object> escortNoLocks = new ArrayList<>() {
         {
             add(new Object());
             add(new Object());
@@ -50,14 +51,15 @@ public class UpdateEscortStateController {
      * @return
      */
     @RequestMapping(value = "updateEscortState", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-    public void Run(@RequestParam("escortNo") String escortNo, @RequestParam("newState") EscortStateEnum newState) {
+    public void UpdateEscortState(@RequestParam("escortNo") String escortNo, @RequestParam("newState") EscortStateEnum newState) {
         // 入参检查
         if (escortNo == null || escortNo.isEmpty()) {
             throw new InvalidParameterException("陪护证号不能为空");
         }
 
         // 加陪护号锁
-        var lock = this.locks.get(Integer.valueOf(escortNo) % 10);
+        var idx = Integer.valueOf(escortNo.substring(escortNo.length() - 2)) % escortNoLocks.size();
+        var lock = this.escortNoLocks.get(idx);
         synchronized (lock) {
             // 执行更新服务
             this.escortService.UpdateEscortState(escortNo, newState);
@@ -69,26 +71,34 @@ public class UpdateEscortStateController {
      */
     @Scheduled(initialDelay = 5 * 60 * 1000, fixedDelay = 5 * 60 * 1000)
     public void AutoRefreshEscortState() {
+        // 获取日志工具
+        var logger = Logger.getLogger(UpdateEscortStateController.class.getName());
+
         // 从数据库中查询所有活跃着的陪护证编号
         var escortCards = this.escortService.QueryAllRegisteredEscortNo();
+
+        // 记录开始日志
+        logger.info(String.format(">> 开始更新，总计 %d 条活跃陪护证", escortCards.size()));
+        var beginDate = new Date();
 
         // 轮训刷新状态
         for (EscortCard escortCard : escortCards) {
             try {
                 // 加陪护号锁
-                var lock = this.locks
-                        .get(Integer.valueOf(escortCard.escortNo.substring(escortCard.escortNo.length() - 3)) % 10);
+                var escortNo = escortCard.escortNo;
+                var idx = Integer.valueOf(escortNo.substring(escortNo.length() - 2)) % escortNoLocks.size();
+                var lock = this.escortNoLocks.get(idx);
                 synchronized (lock) {
                     // 执行更新服务
                     this.escortService.RefreshEscortState(escortCard.escortNo);
                 }
             } catch (Exception e) {
-                Logger.getLogger(UpdateEscortStateController.class.getName()).error(e.getMessage());
+                logger.error(String.format("xx 更新陪护证 %s 的状态时发生错误: %s", escortCard.escortNo, e.getMessage()));
             }
         }
 
         // 记录日志
-        Logger.getLogger(UpdateEscortStateController.class.getName())
-                .info(String.format("更新了 %d 个陪护证的状态", escortCards.size()));
+        var endDate = new Date();
+        logger.info(String.format("<< 实际更新 %d，耗时 %d ms", escortCards.size(), endDate.getTime() - beginDate.getTime()));
     }
 }
