dd66726f4ef9b156694a354bb6358fad26f5e53e
Pengzhan.Qin <935419854@qq.com>
2021-12-24 11:51:31 +0800

    fin

--- src/main/java/com/kaos/his/controller/escort/EscortController.java
+++ src/main/java/com/kaos/his/controller/escort/EscortController.java
@@ -350,7 +350,7 @@ public class EscortController {
     /**
      * 定期自动更新陪护证状态
      */
-    @Scheduled(initialDelay = 50 * 60 * 1000, fixedDelay = 10 * 60 * 1000)
+    @Scheduled(initialDelay = 10 * 60 * 1000, fixedDelay = 10 * 60 * 1000)
     public void AutoUpdateEscortState() {
         // 获取日志工具
         var logger = Logger.getLogger(EscortController.class.getName());
