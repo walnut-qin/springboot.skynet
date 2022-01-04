7fdc6549e5b5c6dd019b4766a8868305f0201c73
Pengzhan.Qin <935419854@qq.com>
2022-01-04 12:11:36 +0800

    添加balancePay

--- src/main/java/com/kaos/his/mapper/fee/FinIpbBalancePayMapper.java
+++ src/main/java/com/kaos/his/mapper/fee/FinIpbBalancePayMapper.java
@@ -1,8 +1,12 @@
 package com.kaos.his.mapper.fee;
 
+import java.util.List;
+
+import com.kaos.his.entity.fee.FinIpbBalancePay;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public interface FinIpbBalancePayMapper {
-    
+    List<FinIpbBalancePay> QueryBalancePays(String invoiceNo);
 }
