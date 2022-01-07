58027634c7082fb1b5aba2b67120cb544b93da78
Pengzhan.Qin <935419854@qq.com>
2022-01-07 09:31:22 +0800

    制作第一个界面

--- src/main/java/com/kaos/his/controller/TestController.java
+++ src/main/java/com/kaos/his/controller/TestController.java
@@ -1,15 +1,13 @@
 package com.kaos.his.controller;
 
-import java.util.Date;
-
 import com.kaos.his.service.DayBalanceService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/webApi")
@@ -19,7 +17,11 @@ public class TestController {
 
     @ResponseBody
     @RequestMapping(value = "test", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
-    public String Run(@RequestParam("param") Date param) {
-        return "test";
+    public ModelAndView Run() {
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("/Hello.html");
+
+        return mv;
     }
 }
