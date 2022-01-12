e37c3454bd955314f1ce81415c73d5696a052b38
Pengzhan.Qin <935419854@qq.com>
2022-01-12 17:34:52 +0800

    添加陪护证微服务框架

---
+++ src/main/java/com/kaos/his/controller/inpatient/escort/EscortController.java
@@ -0,0 +1,98 @@
+package com.kaos.his.controller.inpatient.escort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequestMapping("/ms/inpatient/escort")
+public class EscortController {
+    /**
+     * 接口：日志服务
+     */
+    Logger logger = Logger.getLogger(EscortController.class.getName());
+
+    /**
+     * 20个陪护证状态锁
+     */
+    public static final List<Object> stateLocks = new ArrayList<>() {
+        {
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+        }
+    };
+
+    /**
+     * 10个陪护证动作锁
+     */
+    public static final List<Object> actionLocks = new ArrayList<>() {
+        {
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+        }
+    };
+
+    /**
+     * 10个患者锁
+     */
+    public static final List<Object> patientLocks = new ArrayList<>() {
+        {
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+        }
+    };
+
+    /**
+     * 10个陪护锁
+     */
+    public static final List<Object> helperLocks = new ArrayList<>() {
+        {
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+            add(new Object());
+        }
+    };
+}
