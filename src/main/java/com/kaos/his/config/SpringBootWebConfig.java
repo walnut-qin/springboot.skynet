9e64eb4f33a0c020333e9d6385d7297e1089922a
Pengzhan.Qin <935419854@qq.com>
2022-01-26 15:16:21 +0800

    彻底泛华枚举框架

--- src/main/java/com/kaos/his/config/SpringBootWebConfig.java
+++ src/main/java/com/kaos/his/config/SpringBootWebConfig.java
@@ -1,12 +1,20 @@
 package com.kaos.his.config;
 
+import java.util.List;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 import com.kaos.helper.gson.converter.EnumTypeConverter;
 import com.kaos.inf.IEnum;
 
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.converter.ConverterFactory;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -15,7 +23,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringBootWebConfig implements WebMvcConfigurer {
     /**
-     * 自定义转换器
+     * 自定义转换器（入参转换）
      */
     @Override
     public void addFormatters(FormatterRegistry registry) {
@@ -33,5 +41,25 @@ public class SpringBootWebConfig implements WebMvcConfigurer {
         public <T extends IEnum> Converter<String, T> getConverter(Class<T> targetType) {
             return new EnumTypeConverter<>(targetType);
         }
+    }
+
+    /**
+     * body转换
+     */
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new GsonHttpMessageConverter());
+        converters = Collections2.filter(converters, new Predicate<HttpMessageConverter<?>>() {
+            @Override
+            public boolean apply(@Nullable HttpMessageConverter<?> input) {
+                if (input.getClass().equals(MappingJackson2HttpMessageConverter.class)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }).stream().toList();
+
+        WebMvcConfigurer.super.extendMessageConverters(converters);
     }
 }
