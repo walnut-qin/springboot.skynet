8abc3b07c5bcffe0b3f8f53865308e0a5891f00c
Pengzhan.Qin <935419854@qq.com>
2022-02-10 08:07:33 +0800

    补全opbFeeDetail字段

--- src/main/java/com/kaos/his/entity/outpatient/fee/FinOpbFeeDetail.java
+++ src/main/java/com/kaos/his/entity/outpatient/fee/FinOpbFeeDetail.java
@@ -5,7 +5,10 @@ import java.util.Date;
 import com.kaos.his.entity.common.Department;
 import com.kaos.his.entity.common.Employee;
 import com.kaos.his.entity.outpatient.Outpatient;
+import com.kaos.his.enums.common.MinFeeEnum;
 import com.kaos.his.enums.common.TransTypeEnum;
+import com.kaos.his.enums.outpatient.ClassCodeEnum;
+import com.kaos.his.enums.pharmacy.DrugQualityEnum;
 
 /**
  * 门诊费用明细（XYHIS.FIN_OPB_FEEDETAIL）
@@ -32,6 +35,31 @@ public class FinOpbFeeDetail {
     public String clinicCode = null;
 
     /**
+     * 就诊卡号
+     */
+    public String cardNo = null;
+
+    /**
+     * 挂号日期
+     */
+    public Date regDate = null;
+
+    /**
+     * 挂号科室
+     */
+    public String regDeptCode = null;
+
+    /**
+     * 开方医师
+     */
+    public String docCode = null;
+
+    /**
+     * 开方医师所在科室
+     */
+    public String docDeptCode = null;
+
+    /**
      * 项目编码
      */
     public String itemCode = null;
@@ -40,6 +68,131 @@ public class FinOpbFeeDetail {
      * 项目名称
      */
     public String itemName = null;
+
+    /**
+     * 是否为药品
+     */
+    public Boolean drugFlag = null;
+
+    /**
+     * 规格
+     */
+    public String specs = null;
+
+    /**
+     * 自制药标识
+     */
+    public Boolean selfMade = null;
+
+    /**
+     * 药品性质
+     */
+    public DrugQualityEnum drugQuality = null;
+
+    /**
+     * 剂型
+     */
+    public String doseModelCode = null;
+
+    /**
+     * 最小费用编码
+     */
+    public MinFeeEnum feeCode = null;
+
+    /**
+     * 系统类别
+     */
+    public ClassCodeEnum classCode = null;
+
+    /**
+     * 单价
+     */
+    public Double unitPrice = null;
+
+    /**
+     * 数量
+     */
+    public Double qty = null;
+
+    /**
+     * 草药的付数，其他药品为1
+     */
+    public Integer days = null;
+
+    /**
+     * 频次代码
+     */
+    public String frequencyCode = null;
+
+    /**
+     * 用法代码
+     */
+    public String usageCode = null;
+
+    /**
+     * 用法名称
+     */
+    public String useName = null;
+
+    /**
+     * 院内注射次数
+     */
+    public Integer injectNumber = null;
+
+    /**
+     * 加急标志
+     */
+    public Boolean emergencyFlag = null;
+
+    /**
+     * 样本类型
+     */
+    public String labType = null;
+
+    /**
+     * 检体
+     */
+    public String checkBody = null;
+
+    /**
+     * 每次用量
+     */
+    public Double doseOnce = null;
+
+    /**
+     * 每次用量单位
+     */
+    public String doseUnit = null;
+
+    /**
+     * 基本剂量
+     */
+    public Double baseDose = null;
+
+    /**
+     * 包装数量
+     */
+    public Integer packQty = null;
+
+    /**
+     * 计价单位
+     */
+    public String priceUnit = null;
+
+    /**
+     * 统筹金额
+     */
+    public Double pubCost = null;
+
+    /**
+     * 自付金额
+     */
+    public Double payCost = null;
+
+    /**
+     * 现金金额
+     */
+    public Double ownCost = null;
 
     /**
      * 执行科室编码
@@ -64,6 +217,16 @@ public class FinOpbFeeDetail {
          * 实体：门诊患者
          */
         public Outpatient outpatient = null;
+
+        /**
+         * 开单科室
+         */
+        public Department regDept = null;
+
+        /**
+         * 开方医师
+         */
+        public Employee doctor = null;
 
         /**
          * 实体：执行科室
--- src/test/java/com/kaos/his/mapper/outpatient/fee/FinOpbFeeDetailMapperTests.java
+++ src/test/java/com/kaos/his/mapper/outpatient/fee/FinOpbFeeDetailMapperTests.java
@@ -1,5 +1,6 @@
 package com.kaos.his.mapper.outpatient.fee;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,22 @@ public class FinOpbFeeDetailMapperTests {
 
     @Test
     public void queryFeeDetailsWithClinicCode() {
-        this.finOpbFeeDetailMapper.queryFeeDetailsWithClinicCode("clinicCode", "itemCode", new Date(), new Date());
+        Calendar beginDate = Calendar.getInstance();
+        beginDate.set(Calendar.YEAR, 2013);
+        beginDate.set(Calendar.MONTH, Calendar.SEPTEMBER);
+        beginDate.set(Calendar.DATE, 1);
+        beginDate.set(Calendar.HOUR, 0);
+        beginDate.set(Calendar.MINUTE, 0);
+        beginDate.set(Calendar.SECOND, 0);
+        Calendar endDate = Calendar.getInstance();
+        endDate.set(Calendar.YEAR, 2013);
+        endDate.set(Calendar.MONTH, Calendar.SEPTEMBER);
+        endDate.set(Calendar.DATE, 1);
+        endDate.set(Calendar.HOUR, 23);
+        endDate.set(Calendar.MINUTE, 59);
+        endDate.set(Calendar.SECOND, 59);
+        this.finOpbFeeDetailMapper.queryFeeDetailsWithClinicCode("160670", "F00000005341", beginDate.getTime(),
+                endDate.getTime());
     }
 
     @Test
