c26a457b8d47f06dc840085ffcd7744c86b5ae90
Pengzhan.Qin <935419854@qq.com>
2022-02-14 17:08:31 +0800

    添加挂号主表实体

--- src/main/java/com/kaos/his/entity/outpatient/FinOprRegister.java
+++ src/main/java/com/kaos/his/entity/outpatient/FinOprRegister.java
@@ -3,10 +3,15 @@ package com.kaos.his.entity.outpatient;
 import java.util.Date;
 
 import com.kaos.his.entity.common.DawnOrgDept;
+import com.kaos.his.entity.common.DawnOrgEmpl;
 import com.kaos.his.entity.common.Patient;
+import com.kaos.his.enums.common.DeptOwnEnum;
 import com.kaos.his.enums.common.NoonEnum;
 import com.kaos.his.enums.common.SexEnum;
 import com.kaos.his.enums.common.TransTypeEnum;
+import com.kaos.his.enums.common.ValidStateEnum;
+import com.kaos.his.enums.outpatient.OutpatientStateEnum;
+import com.kaos.his.enums.outpatient.RegisterPayModeEnum;
 
 /**
  * 门诊挂号实体
@@ -23,7 +28,7 @@ public class FinOprRegister {
     public TransTypeEnum transType = null;
 
     /**
-     * 就诊卡号
+     * 就诊卡号 {@link FinOprRegister.AssociateEntity#patient}
      */
     public String cardNo = null;
 
@@ -108,7 +113,7 @@ public class FinOprRegister {
     public String regLevlName = null;
 
     /**
-     * 科室编码
+     * 科室编码 -> {@link FinOprRegister.AssociateEntity#regDept}
      */
     public String deptCode = null;
 
@@ -143,7 +148,7 @@ public class FinOprRegister {
     public Date endTime = null;
 
     /**
-     * 医师编码
+     * 医师编码 {@link FinOprRegister.AssociateEntity#doctor}
      */
     public String doctCode = null;
 
@@ -153,18 +158,383 @@ public class FinOprRegister {
     public String doctName = null;
 
     /**
+     * 挂号是否收费标志
+     */
+    public Boolean ynRegCharge = null;
+
+    /**
+     * 发票号
+     */
+    public String invoiceNo = null;
+
+    /**
+     * 处方号
+     */
+    public String recipeNo = null;
+
+    /**
+     * 0 现场挂号，1 预约挂号，2 特诊挂号
+     */
+    public String ynBook = null;
+
+    /**
+     * 0 复诊，1 出诊，2 住院复诊
+     */
+    public String ynFr = null;
+
+    /**
+     * 是否为加号
+     */
+    public Boolean appenFlag = null;
+
+    /**
+     * 挂号费
+     */
+    public Double regFee = null;
+
+    /**
+     * 检查费
+     */
+    public Double checkFee = null;
+
+    /**
+     * 诊察费
+     */
+    public Double diagFee = null;
+
+    /**
+     * 附加费
+     */
+    public Double otherFee = null;
+
+    /**
+     * 自费金额
+     */
+    public Double ownCost = null;
+
+    /**
+     * 统筹金额
+     */
+    public Double pubCost = null;
+
+    /**
+     * 自付金额
+     */
+    public Double payCost = null;
+
+    /**
+     * 有效标识
+     */
+    public ValidStateEnum validFlag = null;
+
+    /**
+     * 操作员编码 {@link FinOprRegister.AssociateEntity#regEmpl}
+     */
+    public String operCode = null;
+
+    /**
+     * 操作日期
+     */
+    public Date operDate = null;
+
+    /**
+     * 作废人 {@link FinOprRegister.AssociateEntity#cancelEmpl}
+     */
+    public String cancelOperCode = null;
+
+    /**
+     * 作废时间
+     */
+    public String cancelDate = null;
+
+    /**
+     * 医疗类别
+     */
+    public String medType = null;
+
+    /**
+     * ICD编码
+     */
+    public String icdCode = null;
+
+    /**
+     * 审批人 {@link FinOprRegister.AssociateEntity#examEmpl}
+     */
+    public String examCode = null;
+
+    /**
+     * 审批时间
+     */
+    public String examDate = null;
+
+    /**
+     * 核查标志
+     */
+    public Boolean checkFlag = null;
+
+    /**
+     * 核查人 {@link FinOprRegister.AssociateEntity#checkEmpl}
+     */
+    public String checkOperCode = null;
+
+    /**
+     * 核查时间
+     */
+    public String checkOperDate = null;
+
+    /**
+     * 日结标志
+     */
+    public Boolean balanceFlag = null;
+
+    /**
+     * 日结标识号
+     */
+    public String balanceNo = null;
+
+    /**
+     * 日结员 {@link FinOprRegister.AssociateEntity#balanceEmpl}
+     */
+    public String balanceOperCode = null;
+
+    /**
+     * 日结时间
+     */
+    public Date balanceDate = null;
+
+    /**
+     * 是否已看诊
+     */
+    public Boolean ynSee = null;
+
+    /**
+     * 看诊日期
+     */
+    public Date seeDate = null;
+
+    /**
+     * 分诊标识
+     */
+    public Boolean triageFlag = null;
+
+    /**
+     * 分诊护士编码 {@link FinOprRegister.AssociateEntity#triageEmpl}
+     */
+    public String triageOperCode = null;
+
+    /**
+     * 分诊日期
+     */
+    public Date triageDate = null;
+
+    /**
+     * 发票打印次数
+     */
+    public Integer invoicePrintCnt = null;
+
+    /**
+     * 看诊科室代码 {@link FinOprRegister.AssociateEntity#seeDept}
+     */
+    public String seeDeptCode = null;
+
+    /**
+     * 看诊医生编码 {@link FinOprRegister.AssociateEntity#seeDoct}
+     */
+    public String seeDocCode = null;
+
+    /**
+     * 患者来源，4为互联网门诊患者
+     */
+    public String inSource = null;
+
+    /**
+     * 是否需要提取病案
+     */
+    public Boolean sendInHosCaseFlag = null;
+
+    /**
+     * 是否加密姓名
+     */
+    public Boolean encryptNameFlag = null;
+
+    /**
+     * 密文
+     */
+    public String normalName = null;
+
+    /**
+     * 留观开始时间
+     */
+    public Date inDate = null;
+
+    /**
+     * 留观结束时间
+     */
+    public Date outDate = null;
+
+    /**
+     * 转归代号
+     */
+    public String zg = null;
+
+    /**
+     * 状态
+     */
+    public OutpatientStateEnum inState = null;
+
+    /**
+     * 优惠金额
+     */
+    public Double ecoCost = null;
+
+    /**
+     * 账户挂号标识
+     */
+    public Boolean isAccount = null;
+
+    /**
+     * 急诊号
+     */
+    public Boolean emergencyFlag = null;
+
+    /**
+     * 系统自助挂号标志
+     */
+    public String mark1 = null;
+
+    /**
+     * 接诊医生编码 {@link FinOprRegister.AssociateEntity#receiptDoct}
+     */
+    public String receiptDoctCode = null;
+
+    /**
+     * 接诊时间
+     */
+    public Date receiptDate = null;
+
+    /**
+     * 接诊状态
+     */
+    public Boolean receiptFlag = null;
+
+    /**
+     * 门诊一卡通账号
+     */
+    public String accountNo = null;
+
+    /**
+     * EMR挂号流水号
+     */
+    public Integer emrRegId = null;
+
+    /**
+     * 健康之路订单号
+     */
+    public String netCardNo = null;
+
+    /**
+     * 院区标识
+     */
+    public DeptOwnEnum deptOwn = null;
+
+    /**
+     * 职工院区标识
+     */
+    public DeptOwnEnum emplOwn = null;
+
+    /**
+     * 号源顺序，分时段挂号
+     */
+    public Integer schemaQueue = null;
+
+    /**
+     * 支付方式
+     */
+    public RegisterPayModeEnum payMode = null;
+
+    /**
+     * 预约ID
+     */
+    public String recId = null;
+
+    /**
+     * 支付成功订单号
+     */
+    public String payOrderNo = null;
+
+    /**
+     * 客户端订单号
+     */
+    public String clinicOrderId = null;
+
+    /**
+     * 标识已打印
+     */
+    public Boolean isModifyPrint = null;
+
+    /**
      * 关联实体
      */
     public class AssociateEntity {
         /**
-         * 患者实体
+         * 患者实体 {@link FinOprRegister#cardNo}
          */
         public Patient patient = null;
 
         /**
-         * 挂号科室
+         * 挂号科室 {@link FinOprRegister#deptCode}
          */
         public DawnOrgDept regDept = null;
+
+        /**
+         * 看诊医生 {@link FinOprRegister#doctCode}
+         */
+        public DawnOrgEmpl doctor = null;
+
+        /**
+         * 操作员 {@link FinOprRegister#operCode}
+         */
+        public DawnOrgEmpl regEmpl = null;
+
+        /**
+         * 作废人 {@link FinOprRegister#cancelOperCode}
+         */
+        public DawnOrgEmpl cancelEmpl = null;
+
+        /**
+         * 审批人 {@link FinOprRegister#examCode}
+         */
+        public DawnOrgEmpl examEmpl = null;
+
+        /**
+         * 核查人 {@link FinOprRegister#checkOperCode}
+         */
+        public DawnOrgEmpl checkEmpl = null;
+
+        /**
+         * 日结员 {@link FinOprRegister#balanceOperCode}
+         */
+        public DawnOrgEmpl balanceEmpl = null;
+
+        /**
+         * 分诊护士 {@link FinOprRegister#triageOperCode}
+         */
+        public DawnOrgEmpl triageEmpl = null;
+
+        /**
+         * 看诊科室 {@link FinOprRegister#seeDeptCode}
+         */
+        public DawnOrgDept seeDept = null;
+
+        /**
+         * 看诊医生 {@link FinOprRegister#seeDocCode}
+         */
+        public DawnOrgEmpl seeDoct = null;
+
+        /**
+         * 接诊医生 {@link FinOprRegister#receiptDoctCode}
+         */
+        public DawnOrgEmpl receiptDoct = null;
     }
 
     /**
---
+++ src/main/java/com/kaos/his/enums/outpatient/RegisterPayModeEnum.java
@@ -0,0 +1,39 @@
+package com.kaos.his.enums.outpatient;
+
+import com.kaos.inf.IEnum;
+
+public enum RegisterPayModeEnum implements IEnum {
+    账户余额("1", "账户余额"), 工行银行卡("2", "工行银行卡"), 支付宝("3", "支付宝"), 微信("4", "微信"), 现金("5", "现金"), 老医保("6", "老医保"),
+    新医保("7", "新医保"), 诊间银行卡("8", "诊间银行卡"), 建行银行卡("10", "建行银行卡"), 扫码挂号("11", "扫码挂号");
+
+    /**
+     * 数据库存值
+     */
+    private String value;
+
+    /**
+     * 描述存值
+     */
+    private String description;
+
+    /**
+     * 构造
+     * 
+     * @param index
+     * @param description
+     */
+    RegisterPayModeEnum(String value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+}
