f188015d9480fc01c9633a49783066b9c3d323d2
Pengzhan.Qin <935419854@qq.com>
2022-02-15 08:35:48 +0800

    tmp

--- src/main/java/com/kaos/his/service/outpatient/impl/FeeServiceImpl.java
+++ src/main/java/com/kaos/his/service/outpatient/impl/FeeServiceImpl.java
@@ -2,6 +2,8 @@ package com.kaos.his.service.outpatient.impl;
 
 import java.util.List;
 
+import com.kaos.his.enums.common.TransTypeEnum;
+import com.kaos.his.mapper.outpatient.FinOprRegisterMapper;
 import com.kaos.his.mapper.outpatient.fee.FinOpbFeeDetailMapper;
 import com.kaos.his.service.outpatient.FeeService;
 
@@ -17,13 +19,23 @@ public class FeeServiceImpl implements FeeService {
     Logger logger = Logger.getLogger(GcpServiceImpl.class.getName());
 
     /**
-     * 配置列表接口
+     * 挂号接口
+     */
+    @Autowired
+    FinOprRegisterMapper registerMapper;
+
+    /**
+     * 门诊费用接口
      */
     @Autowired
     FinOpbFeeDetailMapper feeDetailMapper;
 
     @Override
     public void undrugPriced(String clinicCode, String operCode, List<String> itemCodes, String termNo) {
-        
+        // 查询挂号记录
+        var register = this.registerMapper.queryRegisterRec(clinicCode, TransTypeEnum.Positive);
+        if (register == null) {
+            throw new RuntimeException("未查询到挂号记录, 无法划价");
+        }
     }
 }
