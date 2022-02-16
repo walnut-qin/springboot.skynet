2656c77331cb3fd6147b54613e5a6fda82bebcb2
Pengzhan.Qin <935419854@qq.com>
2022-02-16 17:29:42 +0800

    增加网络挂号接口

--- src/main/java/com/kaos/his/service/outpatient/RegService.java
+++ src/main/java/com/kaos/his/service/outpatient/RegService.java
@@ -2,6 +2,9 @@ package com.kaos.his.service.outpatient;
 
 import java.util.Date;
 
+import com.kaos.his.enums.common.NoonEnum;
+import com.kaos.his.enums.outpatient.RegisterPayModeEnum;
+
 public interface RegService {
     /**
      * 挂免费号
@@ -11,6 +14,10 @@ public interface RegService {
      * @param deptCode 看诊科室
      * @param docCode  看诊医生
      * @param seeDate  看诊时间
+     * @param noon     午别
+     * @param operCode 挂号操作员
+     * @param payMode  支付方式
      */
-    void freeRegister(String cardNo, String idenNo, String deptCode, String doctCode, Date seeDate);
+    void freeRegister(String cardNo, String deptCode, String doctCode, Date seeDate, NoonEnum noon, String operCode,
+            RegisterPayModeEnum payMode);
 }
--- src/main/java/com/kaos/his/service/outpatient/impl/RegServiceImpl.java
+++ src/main/java/com/kaos/his/service/outpatient/impl/RegServiceImpl.java
@@ -5,12 +5,17 @@ import java.util.Date;
 import com.kaos.his.cache.common.ComPatientInfoCache;
 import com.kaos.his.cache.common.DawnOrgDeptCache;
 import com.kaos.his.cache.common.DawnOrgEmplCache;
+import com.kaos.his.entity.outpatient.FinOprRegister;
+import com.kaos.his.enums.common.NoonEnum;
+import com.kaos.his.enums.common.TransTypeEnum;
 import com.kaos.his.enums.common.ValidStateEnum;
+import com.kaos.his.enums.outpatient.RegisterPayModeEnum;
 import com.kaos.his.mapper.outpatient.FinOprRegisterMapper;
 import com.kaos.his.service.outpatient.RegService;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 public class RegServiceImpl implements RegService {
     /**
@@ -42,8 +47,10 @@ public class RegServiceImpl implements RegService {
     @Autowired
     DawnOrgDeptCache dawnOrgDeptCache;
 
+    @Transactional
     @Override
-    public void freeRegister(String cardNo, String idenNo, String deptCode, String doctCode, Date seeDate) {
+    public void freeRegister(String cardNo, String deptCode, String doctCode, Date seeDate, NoonEnum noon,
+            String opercode, RegisterPayModeEnum payMode) {
         // 获取患者基本信息
         var patientInfo = this.patientInfoCache.getValue(cardNo);
         if (patientInfo == null || patientInfo.isValid != ValidStateEnum.有效) {
@@ -62,6 +69,56 @@ public class RegServiceImpl implements RegService {
             throw new RuntimeException(String.format("未检索到有效医生(%s)基本信息", doctCode));
         }
 
-        // 检索必要序列
+        // 构造挂号记录
+        var register = new FinOprRegister();
+        register.transType = TransTypeEnum.Positive;
+        register.cardNo = cardNo;
+        register.regDate = new Date();
+        register.noon = noon;
+        register.name = patientInfo.name;
+        register.idenNo = patientInfo.identityCardNo;
+        register.sex = patientInfo.sex;
+        register.birthday = patientInfo.birthday;
+        register.cardType = "01";
+        register.payKindCode = "01";
+        register.pactCode = "1";
+        register.pactName = "自费";
+        register.regLevlCode = "06";
+        register.regLevlName = "网络减免号";
+        register.deptCode = deptCode;
+        register.deptName = dept.deptName;
+        register.orderNo = "0";
+        register.beginTime = seeDate;
+        register.endTime = seeDate;
+        register.doctCode = doctCode;
+        register.doctName = doct.emplName;
+        register.ynRegCharge = true;
+        register.recipeNo = "7";
+        register.ynBook = "0";
+        register.ynFr = "1";
+        register.appenFlag = false;
+        register.regFee = 0.0;
+        register.checkFee = 0.0;
+        register.diagFee = 0.0;
+        register.otherFee = 0.0;
+        register.ownCost = 0.0;
+        register.pubCost = 0.0;
+        register.payCost = 0.0;
+        register.validFlag = ValidStateEnum.有效;
+        register.operCode = opercode;
+        register.operDate = new Date();
+        register.checkFlag = false;
+        register.ynSee = false;
+        register.seeDate = seeDate;
+        register.sendInHosCaseFlag = false;
+        register.encryptNameFlag = false;
+        register.ecoCost = 0.0;
+        register.isAccount = false;
+        register.emergencyFlag = false;
+        register.deptOwn = dept.deptOwn;
+        register.payMode = payMode;
+
+        // 插入挂号记录
+        this.registerMapper.insertRegisterRec(register);
     }
 }
