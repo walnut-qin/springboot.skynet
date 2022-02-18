fb8073500d504a361833b526c951fd767517021b
Pengzhan.Qin <935419854@qq.com>
2022-02-18 18:36:42 +0800

    初始添加住院主表

---
+++ src/main/java/com/kaos/his/entity/inpatient/FinIprInMainInfo.java
@@ -0,0 +1,746 @@
+package com.kaos.his.entity.inpatient;
+
+import java.util.Date;
+
+import com.kaos.his.entity.common.ComPatientInfo;
+import com.kaos.his.enums.common.SexEnum;
+
+/**
+ * 住院主表信息(XYHIS.FIN_IPR_INMAININFO)
+ */
+public class FinIprInMainInfo {
+    /**
+     * 住院流水号
+     */
+    public String inpatientNo = null;
+
+    /**
+     *
+     */
+    public String medicalType = null;
+
+    /**
+     * 住院号
+     */
+    public String patientNo = null;
+
+    /**
+     * 就诊卡号 {@link FinIprInMainInfo.AssociateEntity#patientInfo}
+     */
+    public String cardNo = null;
+
+    /**
+     * 社保卡号
+     */
+    public String medCardNo = null;
+
+    /**
+     * 姓名
+     */
+    public String name = null;
+
+    /**
+     * 性别
+     */
+    public SexEnum sex = null;
+
+    /**
+     * 身份证号
+     */
+    public String idenNo = null;
+
+    /**
+     * 拼写码
+     */
+    public String spellCode = null;
+
+    /**
+     * 生日
+     */
+    public Date birthday = null;
+
+    /**
+     * 职业代码
+     */
+    public String profCode = null;
+
+    /**
+     * 工作单位
+     */
+    public String workName = null;
+
+    /**
+     * 工作电话
+     */
+    public String workTel = null;
+
+    /**
+     * 工作邮编
+     */
+    public String workZip = null;
+
+    /**
+     * 家庭住址
+     */
+    public String home = null;
+
+    /**
+     * 家庭电话
+     */
+    public String homeTel = null;
+
+    /**
+     * 家庭邮编
+     */
+    public String homeZip = null;
+
+    /**
+     * 籍贯
+     */
+    public String dist = null;
+
+    /**
+     * 出生地代码
+     */
+    public String birthArea = null;
+
+    /**
+     * 民族
+     */
+    public String nationCode = null;
+
+    /**
+     * 联系人姓名
+     */
+    public String linkmanName = null;
+
+    /**
+     * 联系人电话
+     */
+    public String linkmanTel = null;
+
+    /**
+     * 联系人地址
+     */
+    public String linkmanAddress = null;
+
+    /**
+     * 联系人关系
+     */
+    public String linkmanRelation = null;
+
+    /**
+     *
+     */
+    public String MARI = null;
+
+    /**
+     *
+     */
+    public String COUN_CODE = null;
+
+    /**
+     *
+     */
+    public String HEIGHT = null;
+
+    /**
+     *
+     */
+    public String WEIGHT = null;
+
+    /**
+     *
+     */
+    public String BLOOD_DRESS = null;
+
+    /**
+     *
+     */
+    public String BLOOD_CODE = null;
+
+    /**
+     *
+     */
+    public String HEPATITIS_FLAG = null;
+
+    /**
+     *
+     */
+    public String ANAPHY_FLAG = null;
+
+    /**
+     *
+     */
+    public String IN_DATE = null;
+
+    /**
+     *
+     */
+    public String DEPT_CODE = null;
+
+    /**
+     *
+     */
+    public String DEPT_NAME = null;
+
+    /**
+     *
+     */
+    public String PAYKIND_CODE = null;
+
+    /**
+     *
+     */
+    public String PACT_CODE = null;
+
+    /**
+     *
+     */
+    public String PACT_NAME = null;
+
+    /**
+     *
+     */
+    public String BED_NO = null;
+
+    /**
+     *
+     */
+    public String NURSE_CELL_CODE = null;
+
+    /**
+     *
+     */
+    public String NURSE_CELL_NAME = null;
+
+    /**
+     *
+     */
+    public String HOUSE_DOC_CODE = null;
+
+    /**
+     *
+     */
+    public String HOUSE_DOC_NAME = null;
+
+    /**
+     *
+     */
+    public String CHARGE_DOC_CODE = null;
+
+    /**
+     *
+     */
+    public String CHARGE_DOC_NAME = null;
+
+    /**
+     *
+     */
+    public String CHIEF_DOC_CODE = null;
+
+    /**
+     *
+     */
+    public String CHIEF_DOC_NAME = null;
+
+    /**
+     *
+     */
+    public String DUTY_NURSE_CODE = null;
+
+    /**
+     *
+     */
+    public String DUTY_NURSE_NAME = null;
+
+    /**
+     *
+     */
+    public String IN_CIRCS = null;
+
+    /**
+     *
+     */
+    public String IN_AVENUE = null;
+
+    /**
+     *
+     */
+    public String IN_SOURCE = null;
+
+    /**
+     *
+     */
+    public String IN_TIMES = null;
+
+    /**
+     *
+     */
+    public String PREPAY_COST = null;
+
+    /**
+     *
+     */
+    public String CHANGE_PREPAYCOST = null;
+
+    /**
+     *
+     */
+    public String MONEY_ALERT = null;
+
+    /**
+     *
+     */
+    public String TOT_COST = null;
+
+    /**
+     *
+     */
+    public String OWN_COST = null;
+
+    /**
+     *
+     */
+    public String PAY_COST = null;
+
+    /**
+     *
+     */
+    public String PUB_COST = null;
+
+    /**
+     *
+     */
+    public String ECO_COST = null;
+
+    /**
+     *
+     */
+    public String FREE_COST = null;
+
+    /**
+     *
+     */
+    public String CHANGE_TOTCOST = null;
+
+    /**
+     *
+     */
+    public String UPPER_LIMIT = null;
+
+    /**
+     *
+     */
+    public String FEE_INTERVAL = null;
+
+    /**
+     *
+     */
+    public String BALANCE_NO = null;
+
+    /**
+     *
+     */
+    public String BALANCE_COST = null;
+
+    /**
+     *
+     */
+    public String BALANCE_PREPAY = null;
+
+    /**
+     *
+     */
+    public String BALANCE_DATE = null;
+
+    /**
+     *
+     */
+    public String STOP_ACOUNT = null;
+
+    /**
+     *
+     */
+    public String BABY_FLAG = null;
+
+    /**
+     *
+     */
+    public String CASE_FLAG = null;
+
+    /**
+     *
+     */
+    public String IN_STATE = null;
+
+    /**
+     *
+     */
+    public String LEAVE_FLAG = null;
+
+    /**
+     *
+     */
+    public String PREPAY_OUTDATE = null;
+
+    /**
+     *
+     */
+    public String OUT_DATE = null;
+
+    /**
+     *
+     */
+    public String ZG = null;
+
+    /**
+     *
+     */
+    public String EMPL_CODE = null;
+
+    /**
+     *
+     */
+    public String IN_ICU = null;
+
+    /**
+     *
+     */
+    public String CASESEND_FLAG = null;
+
+    /**
+     *
+     */
+    public String TEND = null;
+
+    /**
+     *
+     */
+    public String CRITICAL_FLAG = null;
+
+    /**
+     *
+     */
+    public String PREFIXFEE_DATE = null;
+
+    /**
+     *
+     */
+    public String OPER_CODE = null;
+
+    /**
+     *
+     */
+    public String OPER_DATE = null;
+
+    /**
+     *
+     */
+    public String BLOOD_LATEFEE = null;
+
+    /**
+     *
+     */
+    public String DAY_LIMIT = null;
+
+    /**
+     *
+     */
+    public String LIMIT_TOT = null;
+
+    /**
+     *
+     */
+    public String LIMIT_OVERTOP = null;
+
+    /**
+     *
+     */
+    public String CLINIC_DIAGNOSE = null;
+
+    /**
+     *
+     */
+    public String PROCREATE_PCNO = null;
+
+    /**
+     *
+     */
+    public String DIETETIC_MARK = null;
+
+    /**
+     *
+     */
+    public String BURSARY_TOTMEDFEE = null;
+
+    /**
+     *
+     */
+    public String MEMO = null;
+
+    /**
+     *
+     */
+    public String BED_LIMIT = null;
+
+    /**
+     *
+     */
+    public String AIR_LIMIT = null;
+
+    /**
+     *
+     */
+    public String BEDOVERDEAL = null;
+
+    /**
+     *
+     */
+    public String EXT_FLAG = null;
+
+    /**
+     *
+     */
+    public String EXT_FLAG1 = null;
+
+    /**
+     *
+     */
+    public String EXT_FLAG2 = null;
+
+    /**
+     *
+     */
+    public String BOARD_COST = null;
+
+    /**
+     *
+     */
+    public String BOARD_PREPAY = null;
+
+    /**
+     *
+     */
+    public String BOARD_STATE = null;
+
+    /**
+     *
+     */
+    public String OWN_RATE = null;
+
+    /**
+     *
+     */
+    public String PAY_RATE = null;
+
+    /**
+     *
+     */
+    public String EXT_NUMBER = null;
+
+    /**
+     *
+     */
+    public String EXT_CODE = null;
+
+    /**
+     *
+     */
+    public String DIAG_NAME = null;
+
+    /**
+     *
+     */
+    public String IS_ENCRYPTNAME = null;
+
+    /**
+     *
+     */
+    public String NORMALNAME = null;
+
+    /**
+     *
+     */
+    public String IDCARDTYPE = null;
+
+    /**
+     *
+     */
+    public String ALTER_TYPE = null;
+
+    /**
+     *
+     */
+    public String ALTER_BEGIN = null;
+
+    /**
+     *
+     */
+    public String ALTER_END = null;
+
+    /**
+     *
+     */
+    public String ALTER_APPROVE_CODE = null;
+
+    /**
+     *
+     */
+    public String ALTER_APPROVE_DATE = null;
+
+    /**
+     *
+     */
+    public String OLDSI_NO = null;
+
+    /**
+     *
+     */
+    public String LOCAL_EXT_FLAG1 = null;
+
+    /**
+     *
+     */
+    public String LOCAL_EXT_FLAG2 = null;
+
+    /**
+     *
+     */
+    public String EMR_INPATIENTID = null;
+
+    /**
+     *
+     */
+    public String IS_SINGLEDIS = null;
+
+    /**
+     *
+     */
+    public String SINGLEDIS_ID = null;
+
+    /**
+     *
+     */
+    public String DISEASENAME = null;
+
+    /**
+     *
+     */
+    public String DISEASECOST = null;
+
+    /**
+     *
+     */
+    public String MATERIALCOST = null;
+
+    /**
+     *
+     */
+    public String DEPTOWN = null;
+
+    /**
+     *
+     */
+    public String IS_GREEN_BALANCE = null;
+
+    /**
+     *
+     */
+    public String SI_PROVINCCODE = null;
+
+    /**
+     *
+     */
+    public String SI_PROVINCNAME = null;
+
+    /**
+     *
+     */
+    public String SI_CITYCODE = null;
+
+    /**
+     *
+     */
+    public String SI_CITYNAME = null;
+
+    /**
+     *
+     */
+    public String LOCAL_EXT_FLAG3 = null;
+
+    /**
+     *
+     */
+    public String GCPINPATIENT = null;
+
+    /**
+     *
+     */
+    public String GREENROAD = null;
+
+    /**
+     *
+     */
+    public String VIP_FLAG = null;
+
+    /**
+     *
+     */
+    public String ERASINPATIENT = null;
+
+    /**
+     *
+     */
+    public String VTE = null;
+
+    /**
+     *
+     */
+    public String VTE_CFM = null;
+
+    /**
+     *
+     */
+    public String CLINIC_CODE = null;
+
+    /**
+     *
+     */
+    public String HAPPEN_NO = null;
+
+    /**
+     *
+     */
+    public String MEDICAL_STAFF = null;
+
+    /**
+     *
+     */
+    public String REMARK = null;
+
+    /**
+     *
+     */
+    public String CASE_APPROVE_FLAG = null;
+
+    /**
+     *
+     */
+    public String CASE_REASON = null;
+
+    /**
+     * 关联实体
+     */
+    public class AssociateEntity {
+        /**
+         * 患者基本信息 {@link FinIprInMainInfo#cardNo}
+         */
+        public ComPatientInfo patientInfo = null;
+    }
+
+    /**
+     * 关联实体
+     */
+    transient public AssociateEntity associateEntity = new AssociateEntity();
+}
