c02e4a5a048a168feb768e617b6851042b81be46
Pengzhan.Qin <935419854@qq.com>
2022-02-22 20:22:32 +0800

    fix

--- src/main/java/com/kaos/his/HisApplication.java
+++ src/main/java/com/kaos/his/HisApplication.java
@@ -3,11 +3,11 @@ package com.kaos.his;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan("com.kaos.his.mapper")
 @SpringBootApplication
-// @EnableScheduling
+@EnableScheduling
 public class HisApplication {
 
 	public static void main(String[] args) {
