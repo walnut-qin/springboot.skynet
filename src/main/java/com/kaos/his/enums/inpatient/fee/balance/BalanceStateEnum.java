f1f7f8c18d6b717ad2ddc7e1f9599050bf3fb1d1
Pengzhan.Qin <935419854@qq.com>
2022-02-22 18:34:52 +0800

    添加模型

---
+++ src/main/java/com/kaos/his/enums/inpatient/fee/balance/BalanceStateEnum.java
@@ -0,0 +1,41 @@
+package com.kaos.his.enums.inpatient.fee.balance;
+
+import com.kaos.inf.IEnum;
+
+/**
+ * 结算标识
+ */
+public enum BalanceStateEnum implements IEnum {
+    未结算("0", "未结算"), 已结算("1", "已结算"), 已结转("2", "已结转");
+
+    /**
+     * 数据库存值
+     */
+    private String value;
+
+    /**
+     * 描述存值
+     */
+    private String description;
+
+    /**
+     * 构造
+     * 
+     * @param index
+     * @param description
+     */
+    BalanceStateEnum(String index, String description) {
+        this.value = index;
+        this.description = description;
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+}
