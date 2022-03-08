1494bf2f7cd13ccbb2338e567312afdc49a20c4c
Pengzhan.Qin <935419854@qq.com>
2022-03-08 13:11:46 +0800

    增加预交金修改接口

--- src/main/java/com/kaos/skynet/api/mapper/inpatient/fee/FinIpbInPrepayMapper.java
+++ src/main/java/com/kaos/skynet/api/mapper/inpatient/fee/FinIpbInPrepayMapper.java
@@ -12,4 +12,14 @@ public interface FinIpbInPrepayMapper {
      * @return
      */
     List<FinIpbInPrepay> queryPrepays(String inpatientNo);
+
+    /**
+     * 变更预交金数值
+     * 
+     * @param inpatientNo 住院流水号, 等于 {@code null} 时，将IS NULL作为条件
+     * @param happenNo    发生序号, 等于 {@code null} 时，将IS NULL作为条件
+     * @param cost        预交金金额
+     * @return 修改记录数量
+     */
+    Integer updatePrepayCost(String inpatientNo, Integer happenNo, Double cost);
 }
