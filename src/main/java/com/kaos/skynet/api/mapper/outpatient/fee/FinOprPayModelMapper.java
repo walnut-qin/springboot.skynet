0dee9b4a48e37c9d580826f1d53fe74724e57c90
Pengzhan.Qin <935419854@qq.com>
2022-03-08 17:48:07 +0800

    fix bug

--- src/main/java/com/kaos/skynet/api/mapper/outpatient/fee/FinOprPayModelMapper.java
+++ src/main/java/com/kaos/skynet/api/mapper/outpatient/fee/FinOprPayModelMapper.java
@@ -9,9 +9,9 @@ public interface FinOprPayModelMapper {
      * 根据卡号/住院号查询交易记录
      * 
      * @param cardNo    开始时间；等于 {@code null} 时，将 IS NULL 作为条件
-     * @param refNum    参照号；等于 {@code null} 时，不作为条件
+     * @param busNo     参照号；等于 {@code null} 时，不作为条件
      * @param invoiceNo 结算发票号；等于 {@code null} 时，不作为条件
      * @return
      */
-    List<FinOprPayModel> queryPayModels(String cardNo, String refNum, String invoiceNo);
+    List<FinOprPayModel> queryPayModels(String cardNo, String busNo, String invoiceNo);
 }
