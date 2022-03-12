be83fbf959c7ea7cdc19ed89563a88336239ba96
Pengzhan.Qin <935419854@qq.com>
2022-03-12 16:50:20 +0800

    添加查询陪护状态的统计接口

--- src/main/java/com/kaos/skynet/api/controller/inf/inpatient/escort/StatisticController.java
+++ src/main/java/com/kaos/skynet/api/controller/inf/inpatient/escort/StatisticController.java
@@ -45,4 +45,74 @@ public interface StatisticController {
      * 到过高风险地区清单
      */
     public String highRiskArea = null;
+
+    /**
+     * 陪护1姓名
+     */
+    public String escort1Name = null;
+
+    /**
+     * 陪护1就诊卡号
+     */
+    public String escort1CardNo = null;
+
+    /**
+     * 陪护1就诊卡号
+     */
+    public String escort1IdenNo = null;
+
+    /**
+     * 陪护1核酸结果
+     */
+    public String escort1NucleicAcidResult = null;
+
+    /**
+     * 陪护1联系电话
+     */
+    public String escort1Tel = null;
+
+    /**
+     * 陪护1健康码
+     */
+    public String escort1HealthCode = null;
+
+    /**
+     * 陪护1行程码
+     */
+    public String escort1TravelCode = null;
+
+    /**
+     * 陪护1高风险标识
+     */
+    public String escort1HighRiskFlag = null;
+
+    /**
+     * 陪护1高风险地区
+     */
+    public String escort1HighRiskArea = null;
+
+    /**
+     * 陪护1姓名
+     */
+    public String escort2Name = null;
+
+    /**
+     * 陪护1就诊卡号
+     */
+    public String escort2CardNo = null;
+
+    /**
+     * 陪护1就诊卡号
+     */
+    public String escort2IdenNo = null;
+
+    /**
+     * 陪护1核酸结果
+     */
+    public String escort2NucleicAcidResult = null;
+
+    /**
+     * 陪护1联系电话
+     */
+    public String escort2Tel = null;
 }
