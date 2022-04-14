675d611636474aee0d8e89f54ba6f5b84f8d83b8
Pengzhan.Qin <935419854@qq.com>
2022-04-14 12:08:55 +0800

    feat: 新增院外审核的核酸结果cache及对应的测试模块

---
+++ src/test/java/com/kaos/skynet/api/cache/inpatient/escort/EscortAnnexChecedCacheTests.java
@@ -0,0 +1,23 @@
+package com.kaos.skynet.api.cache.inpatient.escort;
+
+import com.kaos.skynet.api.cache.Cache;
+import com.kaos.skynet.entity.inpatient.escort.EscortAnnexInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EscortAnnexChecedCacheTests {
+    @Autowired
+    Cache<String, EscortAnnexInfo> cache;
+
+    @Test
+    public void getCacheValue() {
+        var ret = this.cache.getValue("2551917678");
+
+        if (ret == null) {
+            return;
+        }
+    }
+}
