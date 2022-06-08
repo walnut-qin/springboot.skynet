0ded596f8e5606cf02e714b19943c06046d87b58
Pengzhan.Qin <935419854@qq.com>
2022-06-08 16:13:57 +0800

    tag

--- src/main/java/com/kaos/skynet/SkynetApplication.java
+++ src/main/java/com/kaos/skynet/SkynetApplication.java
@@ -3,11 +3,11 @@ package com.kaos.skynet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan({ "com.kaos.skynet.api.mapper", "com.kaos.skynet.api.data.mapper" })
 @SpringBootApplication
-// @EnableScheduling
+@EnableScheduling
 public class SkynetApplication {
 
 	public static void main(String[] args) {
