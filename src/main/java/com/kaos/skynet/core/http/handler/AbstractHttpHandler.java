a98b85e2d4bd07d7ccded90ab453b86f4fdbc396
Pengzhan.Qin <935419854@qq.com>
2022-06-14 17:55:23 +0800

    feat(core): 为网络调用增加计时日志

--- src/main/java/com/kaos/skynet/core/http/handler/AbstractHttpHandler.java
+++ src/main/java/com/kaos/skynet/core/http/handler/AbstractHttpHandler.java
@@ -1,9 +1,13 @@
 package com.kaos.skynet.core.http.handler;
 
+import com.google.common.base.Stopwatch;
+
 import org.springframework.web.client.RestTemplate;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 @AllArgsConstructor
 public abstract class AbstractHttpHandler {
     /**
@@ -22,6 +26,11 @@ public abstract class AbstractHttpHandler {
     Integer port;
 
     /**
+     * 计时器
+     */
+    final Stopwatch timer = Stopwatch.createUnstarted();
+
+    /**
      * 发送post请求
      * 
      * @param <T>
@@ -31,12 +40,24 @@ public abstract class AbstractHttpHandler {
      * @return
      */
     public <T> T postForObject(String url, Object reqBody, Class<T> classOfT) {
-        // 格式化url
-        if (url.startsWith("/")) {
-            url = url.replaceFirst("/", "");
+        // 启动计时器
+        timer.reset();
+        timer.start();
+
+        try {
+            // 格式化url
+            if (url.startsWith("/")) {
+                url = url.replaceFirst("/", "");
+            }
+            url = String.format("http://%s:%d/%s", ip, port, url);
+            return this.restTemplate.postForObject(url, reqBody, classOfT);
+        } finally {
+            // 停止计时
+            timer.stop();
+
+            // 记录日志
+            log.info(String.format("网络调用<%s>耗时<%s>", url, timer.toString()));
         }
-        url = String.format("http://%s:%d/%s", ip, port, url);
-        return this.restTemplate.postForObject(url, reqBody, classOfT);
     }
 
     /**
