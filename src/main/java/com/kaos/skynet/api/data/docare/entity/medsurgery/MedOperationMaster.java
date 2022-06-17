1f72e2a676eb1caf2f2d37c97a4e7508ca510735
Pengzhan.Qin <935419854@qq.com>
2022-06-17 08:50:33 +0800

    feat(data): 添加手麻主表接口

---
+++ src/main/java/com/kaos/skynet/api/data/docare/entity/medsurgery/MedOperationMaster.java
@@ -0,0 +1,150 @@
+package com.kaos.skynet.api.data.docare.entity.medsurgery;
+
+import java.time.LocalDateTime;
+
+import com.google.common.base.Objects;
+import com.kaos.skynet.api.data.docare.enums.SurgeryLevelEnum;
+import com.kaos.skynet.core.type.Enum;
+import com.kaos.skynet.core.type.utils.IntegerUtils;
+import com.kaos.skynet.core.type.utils.StringUtils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MedOperationMaster {
+    /**
+     * 住院号
+     */
+    private String patientId;
+
+    /**
+     * 住院标识，门诊为0
+     */
+    private Integer visitId;
+
+    /**
+     * 手术号
+     */
+    private Integer operId;
+
+    /**
+     * 病人所在科室，即申请科室
+     */
+    private String deptStayed;
+
+    /**
+     * 手术室，即手术科室代码
+     */
+    private String operatingRoom;
+
+    /**
+     * 手术间ID，详见手术间字典
+     */
+    private String operatingRoomNo;
+
+    /**
+     * 术前诊断
+     */
+    private String diagBeforeOperation;
+
+    /**
+     * 病情说明
+     */
+    private String patientCondition;
+
+    /**
+     * 手术等级
+     */
+    private SurgeryLevelEnum operationScale;
+
+    /**
+     * 术后诊断
+     */
+    private String diagAfterOperation;
+
+    /**
+     * 急诊标识<急诊/择期>
+     */
+    private Boolean emergencyIndicator;
+
+    /**
+     * 执行手术的科室
+     */
+    private String operatingDept;
+
+    /**
+     * 手术开始时间
+     */
+    private LocalDateTime startDateTime;
+
+    /**
+     * 手术结束时间
+     */
+    private LocalDateTime endDateTime;
+
+    /**
+     * 手术状态
+     */
+    private OperStatusEnum operStatus;
+
+    /**
+     * 入手术室时间
+     */
+    private LocalDateTime inDateTime;
+
+    /**
+     * 出手术室时间
+     */
+    private LocalDateTime outDateTime;
+
+    /**
+     * 手术排程时间
+     */
+    private LocalDateTime scheduledDateTime;
+
+    @Override
+    public boolean equals(Object arg0) {
+        if (arg0 instanceof MedOperationMaster) {
+            var that = (MedOperationMaster) arg0;
+            return StringUtils.equals(this.patientId, that.patientId)
+                    && IntegerUtils.equals(this.visitId, that.visitId)
+                    && IntegerUtils.equals(this.operId, that.operId);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(patientId, visitId, operId);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum OperStatusEnum implements Enum {
+        手术取消("-80", "手术取消"),
+        准备手术("0", "准备手术"),
+        入手术室("5", "入手术室"),
+        麻醉开始("10", "麻醉开始"),
+        手术开始("15", "手术开始"),
+        手术结束("25", "手术结束"),
+        麻醉结束("30", "麻醉结束"),
+        出手术室("35", "出手术室"),
+        准备复苏("40", "准备复苏"),
+        入复苏室("45", "入复苏室"),
+        出复苏室("55", "出复苏室"),
+        转入病房("60", "转入病房");
+
+        /**
+         * 数据库存值
+         */
+        private String value;
+
+        /**
+         * 描述存值
+         */
+        private String description;
+    }
+}
---
+++ src/main/java/com/kaos/skynet/api/data/docare/mapper/medsurgery/MedOperationMasterMapper.java
@@ -0,0 +1,54 @@
+package com.kaos.skynet.api.data.docare.mapper.medsurgery;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.baomidou.dynamic.datasource.annotation.DS;
+import com.kaos.skynet.api.data.docare.entity.medsurgery.MedOperationMaster;
+import com.kaos.skynet.api.data.docare.entity.medsurgery.MedOperationMaster.OperStatusEnum;
+
+import lombok.Getter;
+import lombok.Builder;
+
+@DS("docare")
+public interface MedOperationMasterMapper {
+    /**
+     * 主键查询
+     * 
+     * @param patientId 住院号, 值为 {@code null} 时, 将 IS NULL 设置为条件
+     * @param visitId   一次入院的手术次序号, 值为 {@code null} 时, 将 IS NULL 设置为条件
+     * @param operId    手术编号, 值为 {@code null} 时, 将 IS NULL 设置为条件
+     * @return
+     */
+    MedOperationMaster queryOperationMaster(String patientId, Integer visitId, Integer operId);
+
+    /**
+     * 批量查询手术
+     * 
+     * @param key
+     * @return
+     */
+    List<MedOperationMaster> queryOperationMasters(Key key);
+
+    /**
+     * 查询键值
+     */
+    @Getter
+    @Builder
+    public static class Key {
+        /**
+         * 手术状态清单
+         */
+        private LocalDateTime beginInDateTime;
+
+        /**
+         * 手术状态清单
+         */
+        private LocalDateTime endInDateTime;
+
+        /**
+         * 手术状态清单
+         */
+        private List<OperStatusEnum> operStatus;
+    }
+}
