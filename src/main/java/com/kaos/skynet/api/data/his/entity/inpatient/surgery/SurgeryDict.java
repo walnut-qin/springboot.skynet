95fcb4fa7772214b5f540eb5539ec0dce35ef3e7
nyakori <935419854@qq.com>
2022-06-28 10:05:50 +0800

    fix(data): 手术字典补全手术分级字段

--- src/main/java/com/kaos/skynet/api/data/his/entity/inpatient/surgery/SurgeryDict.java
+++ src/main/java/com/kaos/skynet/api/data/his/entity/inpatient/surgery/SurgeryDict.java
@@ -3,8 +3,10 @@ package com.kaos.skynet.api.data.his.entity.inpatient.surgery;
 import java.time.LocalDateTime;
 
 import com.google.common.base.Objects;
+import com.kaos.skynet.core.type.Enum;
 import com.kaos.skynet.core.util.StringUtils;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -20,6 +22,11 @@ public class SurgeryDict {
      * 手术名称
      */
     private String surgeryName;
+
+    /**
+     * 手术等级
+     */
+    private SurgeryLevelEnum surgeryLevel;
 
     /**
      * 有效性标识
@@ -43,5 +50,21 @@ public class SurgeryDict {
     @Override
     public int hashCode() {
         return Objects.hashCode(this.icdCode);
+    }
+
+    /**
+     * 手术等级
+     */
+    @Getter
+    @AllArgsConstructor
+    public enum SurgeryLevelEnum implements Enum {
+        一级("1", "一级"),
+        二级("2", "二级"),
+        三级("3", "三级"),
+        四级("4", "四级");
+
+        private String value;
+
+        private String description;
     }
 }
